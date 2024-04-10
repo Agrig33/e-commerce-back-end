a327b848aa6f21c071fa24b8894962b7e3c38d80
{airin grigorian} <agrigorian33@gmail.com>
2024-04-10 01:34:34 -0700

    revised product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,17 +108,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-  .then((deletedProduct) => {
-    res.json(deletedProduct);
-  })
-  .catch((err) => res.json(err));
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+ 
+
 
 module.exports = router;
