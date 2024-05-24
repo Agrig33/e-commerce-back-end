83004cd93598f34bc90bc71e457b8c7cfcb92f5d
{airin grigorian} <agrigorian33@gmail.com>
2024-05-23 20:00:35 -0700

    revised some syntax and added function to server.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag}],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(req.params.id, {
+    const tagData = await Tag.findOne(req.params.id, {
       include: [{ model: Product }],
     });
     res.status(200).json(tagData);
@@ -34,17 +34,16 @@ router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(201).json(tagData);
-  }catch (err) {
+  } catch (err) {
     res.status(400).json(err);
   }
 });
+
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
+      where: {id: req.params.id},
     });
     
     const updatedTag = await Tag.findByPk(req.params.id);
