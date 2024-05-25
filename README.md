5d4d265efee3b10005e4a2b8926633dbb9b903f4
{airin grigorian} <agrigorian33@gmail.com>
2024-05-24 17:23:48 -0700

    revised some things with product-routes.js

--- README.md
+++ README.md
@@ -1,7 +1,52 @@
-# E-commerce-back-end
+# E-Commerce-Back-End ## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
-This was created as a demonstration showcasing the back end for an e-commerce site.
+This was created to demonstrate the back end for an e-commerce site.
+
+## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Video Tutorial](#tutorial)
+  - [Contributing](#contributing)
+  - [License](#License)
+  - [Questions](#questions)
+
+## Installation
+
+  1. Clone the repository 
+  2. Go to project directory and open in VS Code
+  3. Open terminal and install dependencies (npm install, npm i sequelize, )
+        *npm install
+        *npm i sequelize
+        *npm dotenv
+        *run express
+  4. Run application - 'node server'
+
+## Usage
+  1. Run npm install
+  2. Use mysql -u root to connect to database
+  3. Source schema.sql
+  4. Enter/Run npm run seed to seed data to your database
+  5. Run node server
+  6. Open localhost in Insomnia
+
+## Video Tutorial
+<b>[See Demo] </b>
+
+## Contributions
+
+1. Fork Repository
+2. Create a new branch for your changes
+3. Make your changes and revisions
+4. Submit a pull request
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Questions
+If you have questions, please feel free to contact me [Agrig33](https://github.com/Agrig33). 
 
 ## Mock Up
 ![Alt Text](./assets/13-orm-homework-demo-01.gif)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,58 +1,52 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
-
 // get all products
-router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-  try {
-    const productData = await Product.findAll({
-      include: [{ model: Category }, {model: Tag, through: ProductTag},
+router.get('/',(req, res) => {
+  Product.findAll({
+      include: [
+        Category,
         {
           model: Tag,
           through: ProductTag,
         },
       ],
     })
-    res.status(200).json(productData);
-  } catch (err) {
+      .then((productData) => res.json(productData))
+    .catch ((err) => {
       console.log(err);
       res.status(500).json(err);
-  }
+  });
 });
 
 // get one product
-router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-  try {
-    const productData = await Product.findOne(req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag,
-  },
-],
-    })
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/:id', (req, res) => {
+  Product.findOne({
+    where:{
+      id: req.params.id,
+    },
+    include: [
+      Category,
+    {
+      model: Tag,
+      through: ProductTag,
+    },
+    ],
+  })
+  .then((productData) => res.json(productData))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+ 
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -111,7 +105,7 @@ router.put('/:id', (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
